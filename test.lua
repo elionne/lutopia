37482693b785257ebc3385b232dd87358a64b2d6
Elionne <elionne@yieu.eu>
2012-02-01 22:43:23 +0100

    [test] adds flashing and gradient in test

--- test.lua
+++ test.lua
@@ -18,24 +18,31 @@ print('un1.group1.test1', u.group1.test1);
 
 a = rgb.new(0.25, 0.5, 0.75);
 b = rgb.new(0.6, 0.1, 0.2);
-blanc = rgb.new(0.1, 0.1, 0.1);
+blanc = rgb.new(1, 1, 1);
 u.test1.rgb = blanc;
 
-for k,v in pairs(u.test1) do print(k, v) end
+--for k,v in pairs(u.test1) do print(k, v) end
 
-print(u.test1.addr);
-print(u.test1.rgb);
+--print(u.test1.addr);
+--print(u.test1.rgb);
 
-print(a:to_html())
-for i=1, 9 do
-	d = light.gradiant(a, b, i/10);
-	print(d:to_html());
-end
-print(b:to_html())
+--print(a:to_html())
+--for i=1, 9 do
+--	d = light.gradiant(a, b, i/10);
+--	print(d:to_html());
+--end
+--print(b:to_html())
 
 function main(p)
-    --print(p);
     --u.test1.rgb:from_hsv{h=p, s=1, v=1};
-    u.test1.rgb = light.gradiant(a, b, p, 0.3)
+
+    if math.random() * 10 < 2 then
+        u.test1.rgb:from_hsv{h=p, s=1, v=1};
+    else
+        u.test1.rgb:from_hsv{h=0, s=0, v=0};
+    end
+
+
+    --u.test1.rgb = light.gradiant(a, b, p, 0.3)
     --print(u.test1.rgb);
 end
